64590394b31a4ee98b3d9a2afccf268e81090271
Muhammet Ali Tanrıkulu <muhammedd45@icloud.com>
2024-12-02 12:40:29 +0300

    Final

--- MuratHocaGithub/MyPortolioUdemy-master/MyPortolioUdemy-master/MyPortolioUdemy/DAL/Entities/Feature.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyPortolioUdemy.DAL.Entities
-{
-    public class Feature
-    {
-        public int FeatureId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
